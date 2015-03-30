c2de617112d542ebb90ea7699ca8096b9f80065e
Dominic Michael Joshua Dunnett <domdunnett@gmail.com>
2015-03-30 22:00:50 +0800

    intial commit

--- js/application.js
+++ js/application.js
@@ -1,9 +1,77 @@
 $(document).ready(function(){
+	
+	var number1;
+	var number2;
+	var randomQuestion;
+	var userAnswer;
+	var randomAnswer;
+	var secondsRemaining = 10;
 
+	function createRandomQuestion() {
+
+		number1 = Math.ceil(Math.random()*10);
+		
+		number2 = Math.ceil(Math.random()*10);
+
+		randomQuestion = number1 + " + " + number2;
+		randomAnswer = eval(randomQuestion)
+
+		$('#question').text(randomQuestion);
+
+	}
+
+	function startTimer() {
+	
+		var timer = setInterval(functionEverySecond, 1000);
+		
+	}
+
+
+	function isAnswerCorrect(userAnswer, answer) {
+		if (userAnswer === answer) {
+			addSeconds();
+			$('#user-input').val('');
+			$('#user-input').parent().removeClass('has-error');
+			createRandomQuestion();
+		}
+		else {
+			$('#user-input').parent().addClass('has-error');
+		}
+	}
+
+  function addSeconds() {
+    secondsRemaining += 2;
+  }
+
+  function functionEverySecond() {
+    if (secondsRemaining < 0) {
+      
+      clearInterval(timer);
+    } else {
+      $('#timer').text(secondsRemaining);
+      secondsRemaining--;
+    }
+  }
+
+
+	createRandomQuestion();
+
+	$('#play').click(function() {
+		$('#user-input').attr('id', 'user-input');
+		$('#user-input').removeAttr('disabled');
+		$('#user-input').removeAttr('placeholder');
+		$('#user-input').focus();
+	});
+	
+	$(document).one('focus', '#user-input', function() {
+		startTimer();
+	});
+
+	$(document).on('keyup', '#user-input', function() {
+		userAnswer = Number($('#user-input').val());
+		answer = eval($('#question').text());
+		isAnswerCorrect(userAnswer, answer);
+	});
+
+	
 });
-
-// for (var name in myObject) {
-//   if (myObject.hasOwnProperty(name)) {
-//     alert(name);
-//   }
-// }
