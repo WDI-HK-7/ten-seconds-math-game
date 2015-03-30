77be12ae3dec229f62c6974fdfe421695619393c
“mia” <mia_dacamara@Mias-MacBook-Pro.local>
2015-03-30 22:15:53 +0800

    JS added game_basic functions

--- js/application.js
+++ js/application.js
@@ -1,6 +1,37 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-});
+  var game = {
+    operation: ['+'],
+    maxNumber: 100,
+    maxInitialTime: 0,
+    timeIncrement: 1
+  }
+
+  var randomNumberOne = Math.ceil(Math.random() * game.maxNumber);
+  var randomNumberTwo = Math.ceil(Math.random() * game.maxNumber);
+
+  function generateSum() {
+    return (randomNumberOne + game.operation + randomNumberTwo);
+    console.log(randomNumberOne + randomNumberTwo);
+  };
+
+  // store the question and answer 
+  var userInput = $('#userInput');
+  var answer = randomNumberOne + randomNumberTwo;
+
+  // compare if userInput is correct
+  function compare() {
+    if (userInput === answer) {
+      console.log("Correct");
+    }
+    console.log("Incorrect");
+  }
+
+  // if correct answer, add 1 second
+
+  // time keeper
+
+// });
 
 // for (var name in myObject) {
 //   if (myObject.hasOwnProperty(name)) {
